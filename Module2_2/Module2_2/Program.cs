5a90be4dbf14c996e06ddfb9eb0357c806781fa6
YuriABC <yhuliashou@gmail.com>
2019-10-16 18:45:14 +0300

    Module 2 task 2 was complete.

---
+++ Module2_2/Module2_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message(InputInt());
+            Console.ReadLine();
+        }
+
+        public static int InputInt()
+        {
+            int n;
+            Console.WriteLine("Enter age");
+            while (!(int.TryParse(Console.ReadLine(), out n)))
+            {
+                Console.WriteLine("Incorrect data.Try again.");
+            }
+            return n;
+        }
+
+        public static void Message(int n)
+        {
+                if ((n >=18) && (n % 2 == 0))
+                    Console.WriteLine("Congratulations on coming of age");
+                else if ((n < 18) && (n > 13) && (n % 2 == 1))
+                Console.WriteLine("Congratulations on coming to high school");
+                else Console.WriteLine("No text for this value");
+        }
+    }
+}
